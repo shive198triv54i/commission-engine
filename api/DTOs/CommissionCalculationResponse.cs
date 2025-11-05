582002874bdbf042f2adf6b1a00e0cecd01b74b7
shivesh.trivedi <shivesh.trivedi@intimetec.com>
2025-11-05 22:54:59 +0530

    feat(api): add CommissionRequest/Response DTOs

---
+++ api/DTOs/CommissionCalculationResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AvalphaTechnologies.CommissionCalculator.DTOs
+{
+    public class CommissionCalculationResponse
+    {
+        public decimal AvalphaTechnologiesCommissionAmount { get; set; }
+
+        public decimal CompetitorCommissionAmount { get; set; }
+    }
+}
